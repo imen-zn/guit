966cfb4c0e49685a76089ed467a0c52817c111e7
imen-zn <zanimen7@gmail.com>
2024-04-05 19:46:21 +0200

    added features

--- src/app/technicien/technicien.module.ts
+++ src/app/technicien/technicien.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TechnicienComponent } from './technicien.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+
 import { ProfileTechComponent } from './profile-tech/profile-tech.component';
-import { FooterComponent } from './footer/footer.component';
+
 import { RouterModule } from '@angular/router';
 
 
@@ -14,9 +14,9 @@ import { RouterModule } from '@angular/router';
   declarations: [
     TechnicienComponent,
     NavbarComponent,
-    SidebarComponent,
+
     ProfileTechComponent,
-    FooterComponent,
+ 
  
   ],
   imports: [
